a00505184a02fe71fb7c8be62ed5ced91c20e7a5
PC <PC@PC-PC>
2018-04-07 23:44:04 +0200

    Úvodní commit

---
+++ ukolVyukovaHra/src/ui/HomeController.java
@@ -0,0 +1,30 @@
+package ui;
+
+	
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * 
+ * @author Bušek Tomáš
+ *
+ */
+
+public class HomeController extends Application {
+	
+
+    public static void main(String[] args) {
+       launch(args);
+   }
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+        
+        
+	
+	
